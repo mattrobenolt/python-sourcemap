8d233a5643a2d1f2a2e59288a05d472f2a7d7412
Matt Robenolt <matt@ydekproductions.com>
2013-03-04 22:46:04 -0800

    0.1.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ tests_require = []
 
 setup(
     name='sourcemap',
-    version='0.1.0',
+    version='0.1.1',
     author='Matt Robenolt',
     author_email='matt@ydekproductions.com',
     url='https://github.com/mattrobenolt/python-sourcemap',
