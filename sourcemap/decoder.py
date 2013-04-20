a24f966a014f94e2205bed2a860190e64f524638
Matt Robenolt <matt@ydekproductions.com>
2013-04-20 13:32:23 -0700

    lol python3

--- sourcemap/decoder.py
+++ sourcemap/decoder.py
@@ -105,7 +105,7 @@ class SourceMapDecoder(object):
         smap = json.loads(source)
         sources = smap['sources']
         sourceRoot = smap.get('sourceRoot')
-        names = map(str, smap['names'])
+        names = list(map(str, smap['names']))
         mappings = smap['mappings']
         lines = mappings.split(';')
 
